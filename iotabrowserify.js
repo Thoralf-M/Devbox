9b4c6b876908504140d17e34604e5967b507cdf3
Thoralf-M <thoralf.mue@gmail.com>
2019-08-17 13:19:26 +0200

    change local pow and split code into files

---
+++ iotabrowserify.js
@@ -0,0 +1,11 @@
+//command: browserify iotabrowserify.js --standalone window| uglifyjs > iota.js-browser.js
+
+global.bundle_validator = require('@iota/bundle-validator');
+global.bundle = require('@iota/bundle');
+global.checksum = require('@iota/checksum');
+global.converter = require('@iota/converter');
+global.core = require('@iota/core');
+global.http_client = require('@iota/http-client');
+global.signing = require('@iota/signing');
+global.transaction_converter = require('@iota/transaction-converter');
+global.transaction = require('@iota/transaction');
